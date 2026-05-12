d3101bc2fde080c77d41b6d55878aaa6c53b0ee0
Fauzan Adhim Mubarak <f1d02510113@student.unram.ac.id>
2026-05-12 20:48:35 +0800

    Jurnal sementara

---
+++ MODUL-4/M4_Soal.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+
+void bannerAwal (){
+    cout << "|==============================================|" << endl;
+    cout << "|                                              |" << endl;
+    cout << "[             MODULE 4 - FUNCTION              ]" << endl;
+    cout << "|                                              |" << endl;
+    cout << "|==============================================|" << endl; 
+    cout << "1. TIVAIZ Store" << endl;
+    cout << "2. Kalkulator TIVAIZ" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "=====================================" << endl;
+}
+int pilihMenu (){
+    cout << "Pilihan menu: ";
+
+
+    return 0;
+}
+void bannerTivaiz (){
+    cout << "#===============================================#" << endl;
+    cout << "|                 TIVAIZ STORE                  |" << endl;
+    cout << "#===============================================#" << endl;
+
+}
+
+int main (){
+    bannerAwal ();
+    pilihMenu ();
+
+ 
+
+    return 0;
+}
