3bdf542d3df74f80550b5bf50307f919046d89dc
Fauzan Adhim Mubarak <f1d02510113@student.unram.ac.id>
2026-05-26 20:47:16 +0800

    Jurnal sementara modul 6

---
+++ MODUL-6/M6_Soal.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void menuUtama(){
+cout << "   CYRON TERMINAL: DIVINE SUPPRESSION  " << endl << endl;
+
+cout << "[1;35mXelisa: "Exquisite... the synchronization is perfect.""
+cout << "------------------------------------------------------------"
+cout << "1. View Neural Map (Status)"
+cout << "2. Inject Neural Thread (Add)"
+cout << "3. Purge Corrupted Link (Delete)"
+cout << "4. Expand Willpower (Resize)"
+cout << "0. Surrender (Exit)"
+cout << "------------------------------------------------------------"
+cout << "Select Operation: "
+
+}
+int main (int argc, char const *argv[]){
+//jujur maaf skill issue kesekia kalinya
+    return 0;
+}
